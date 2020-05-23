44eee4ddd42e6c019aefd66dc2d7ce15b613f3b6
Shahil Pusarla <shpusarl@microsoft.com>
2020-05-23 18:39:01 +0530

    Added New Photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -187,7 +187,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/Shahil_Photo.jpg'
+        icon: 'static/photo.jpg'
       },
     },
     'gatsby-plugin-offline',
